979aec968a48671fef3a518d973c602cf71b202a
Danielflorezva1 <daniel199938@gmail.com>
2022-08-14 18:33:35 -0400

    cambio alas fuentes del form

--- src/components/SectionForm/InputForm.js
+++ src/components/SectionForm/InputForm.js
@@ -1,8 +1,8 @@
 
 
 /////style
-const styleFormInput = 'w-full h-10 my-2 rounded-lg px-3 text-white bg-purple-900 border-2 border-slate-900 placeholder:text-white  placeholder:font-medium' ;
-const styleFormLabel = 'w-10/12';
+const styleFormInput = 'w-full h-10 my-2 rounded-lg px-3 text-cyan-50 bg-purple-900 border-2 border-slate-900 placeholder:text-cyan-200  placeholder:font-medium' ;
+const styleFormLabel = 'w-10/12 text-cyan-50';
 
 ////component
 function InputForm({typeInput, value, info}){
