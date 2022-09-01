76d8644dfb41d5126f2f75f2d5a8fbe61e1e7822
Danielflorezva1 <daniel199938@gmail.com>
2022-09-01 18:28:34 -0400

    cambios suaves

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
+/////importacion  de componentes
+import Header from "./components/Header/Header";
+import SectionDevelopment from "./components/SectionDevelopment/SectionDevelopment";
+import SectionSkills from "./components/SectionSkills/SectionSkills";
+import SectionArticle from "./components/SectionArticle/SectionArticle";
+import SectionForm from "./components/SectionForm/SectionForm";
+import Aside from "./components/SectionAside/Aside";
+import Footer from "./components/SectionFooter/Footer";
+
+/////render 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-    <App />
+    <Header />
+    <SectionDevelopment />
+    <SectionSkills />
+    <SectionArticle />
+    <SectionForm />
+    <Aside />
+    <Footer />
   </>
 );
